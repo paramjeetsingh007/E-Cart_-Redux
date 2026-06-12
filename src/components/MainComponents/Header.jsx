c3169f1f34dd351448848c2290b743551717c847
paramjeetsingh007 <paramjeetsingh121223@gmail.com>
2026-06-12 10:33:18 +0530

    inital commit

---
+++ src/components/MainComponents/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="w-full h-16 bg-[var(--background)] flex items-center justify-between">
+      Header
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/SmallComponents/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='text-[var(--text)] text-2xl font-bold'>
+        Digital E-Cart
+    </div>
+  )
+}
+
+export default Logo
